5588d00189821bbc52ee3f3cc3976ad0261a3cdf
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2024-10-12 23:44:36 -0300

    x

--- design-system/src/components/Button.vue
+++ design-system/src/components/Button.vue
@@ -24,4 +24,15 @@
   },
   };
   </script>
+
+<style scoped>
+.btn {
+  background-color: #42b883;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
   
---
+++ design-system/src/index.js
@@ -0,0 +1,5 @@
+// src/index.js
+import Button from './components/Button.vue';
+import './styles/main.css';
+
+export { Button };
